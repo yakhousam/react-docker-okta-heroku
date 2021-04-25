fab9e4f0e2b7f8f8b6ac0c5ff3bdcb18a5b80e1e
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-04-25 12:47:09 +0100

    added env variables

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 function App() {
   return (
     <Router>
-      <Security issuer='https://dev-10419792.okta.com/oauth2/default'
-                clientId='0oan96bpfnTrhJj5a5d6'
+      <Security issuer={`https://${process.env.REACT_APP_DOMAIN}/oauth2/default`}
+                clientId={`${process.env.REACT_APP_CLIENT_ID}`}
                 redirectUri={window.location.origin + '/callback'}
                 pkce={true}>
         <SecureRoute path='/' exact={true} component={Calendar}/>
